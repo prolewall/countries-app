31ed602a169648e536d0fd30da73dd10d3332d6a
prolewall <h.kowalik98@gmail.com>
2023-08-24 22:15:08 +0200

    add navigation to home page from header

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { FC } from "react";
 
 import ThemeToggle from "../ThemeToggle/themeToggle";
@@ -6,7 +7,12 @@ import styles from "./header.module.css";
 const Header: FC = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Where in the world?</h1>
+      <h1 className={styles.title}>
+        <Link href={"/"} className={styles.link}>
+          Where in the world?
+        </Link>
+      </h1>
+
       <ThemeToggle />
     </header>
   );
