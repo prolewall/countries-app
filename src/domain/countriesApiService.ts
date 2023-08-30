5765194b6a990f4a5a72a05cee4e6d7037b5568a
prolewall <h.kowalik98@gmail.com>
2023-08-30 14:07:09 +0200

    fix bug with countries with only one border not showing any border countries

--- src/domain/countriesApiService.ts
+++ src/domain/countriesApiService.ts
@@ -15,7 +15,7 @@ const COUNTRY_INFO_FIELDS =
 const COUNTRY_DETAILS_FIELDS =
   "name,population,region,capital,cca3,flags,tld,currencies,subregion,languages,borders" as const;
 
-const BORDER_COUNTRIES_FIELDS = "cca3, name" as const;
+const BORDER_COUNTRIES_FIELDS = "cca3,name" as const;
 
 function determineEndpoint(searchValue: string, regionFilter: string): string {
   if (searchValue) {
@@ -71,9 +71,14 @@ export async function getCountryDetails(
 async function getBorderCountries(
   codes: string[]
 ): Promise<BorderCountryInfo[]> {
+  if (codes.length === 0) {
+    return [];
+  }
   const requestCodes = codes.join(",");
+  // currently there is a bug in Countries API where ?fields do not work properly in combination with ?codes
+  // selected fields are ignored and all fields are fetched instead
   const response = await fetch(
-    `${API_BASE}/alpha/?codes=${requestCodes}?fields=${BORDER_COUNTRIES_FIELDS}`
+    `${API_BASE}/alpha/?codes=${requestCodes}&?fields=${BORDER_COUNTRIES_FIELDS}`
   );
   if (response.ok) {
     const data: ApiCountry[] = await response.json();
@@ -82,8 +87,6 @@ async function getBorderCountries(
       name: country.name.common,
       code: country.cca3,
     }));
-  } else if (response.status === 404) {
-    return [];
   } else {
     throw new Error("Something went wrong");
   }
